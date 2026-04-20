611e3a36659ccb53605e35ef110f28db5a1e734f
poornima20 <poornima20suresh@gmail>
2026-04-20 12:05:11 +0530

    topbar responsive

--- script.js
+++ script.js
@@ -81,3 +81,9 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 });
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js")
+    .then(() => console.log("SW registered"))
+    .catch(err => console.log("SW error", err));
+}
